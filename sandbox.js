a3cac23c467a296214c82b81218c5a72dba3d2fa
Umut Akin <umutakin@fastmail.com>
2021-04-13 17:21:00 +0300

    async javascript tutorial 05 - using json data

--- sandbox.js
+++ sandbox.js
@@ -3,13 +3,15 @@ const getTodos = (callback) => {
 
   request.addEventListener("readystatechange", () => {
     if (request.readyState === 4 && request.status === 200) {
-      callback(undefined, request.responseText);
+        const data = JSON.parse(request.responseText)
+        callback(undefined, data);
     } else if (request.readyState === 4) {
       callback('could not fetch data', undefined);
     }
   });
 
   request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+//   request.open('GET', './todos.json'); // CORS :(
   request.send();
 };
 
