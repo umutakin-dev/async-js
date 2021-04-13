d64e1e18e9a34ef4a3350b61de263456096b5461
Umut Akin <umutakin@fastmail.com>
2021-04-13 17:10:31 +0300

    async javascript tutorial 04 - callback functions

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,29 @@
-const request = new XMLHttpRequest();
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-    //console.log(request, request.readyState);
+  request.addEventListener("readystatechange", () => {
     if (request.readyState === 4 && request.status === 200) {
-        console.log(request, request.responseText);
-    } else if(request.readyState === 4) {
-        console.log('could not fetch the data');
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback('could not fetch data', undefined);
+    }
+  });
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  request.send();
+};
+
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {
+    console.log('callback fired');
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
     }
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+console.log(3);
+console.log(4);
