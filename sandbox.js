2d72e2f1cbdb951a23f3ab06db496da4eb37ecd8
Umut Akin <umutakin@fastmail.com>
2021-04-14 11:05:03 +0300

    async javascript tutorial 09 - the fetch api

--- sandbox.js
+++ sandbox.js
@@ -1,33 +1,13 @@
-const getTodos = (resource) => {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
+// fetch api
 
-        request.addEventListener("readystatechange", () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                resolve(data);
-            } else if (request.readyState === 4) {
-                reject("error getting resource");
-            }
-        });
-
-        request.open("GET", resource);
-        request.send();
-    });
-};
-
-getTodos("https://jsonplaceholder.typicode.com/todos/1")
-    .then(data => {
-        console.log("promise 1 resolved", data);
-        return getTodos("https://jsonplaceholder.typicode.com/todos/2");
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => {
+        console.log("resolved", response);
+        return response.json(); // promise
     })
     .then(data => {
-        console.log("promise 2 resolved", data);
-        return getTodos("https://jsonplaceholder.typicode.com/todos/3");
-    })
-    .then(data => {
-        console.log("promise 3 resolved", data);
+        console.log(data);
     })
     .catch((err) => {
-        console.log("promise rejected", err);
+        console.log("rejected", err);
     });
