f116b95170bb56b43ad6cd9457ef00d096f240d2
Umut Akin <umutakin@fastmail.com>
2021-04-14 11:26:06 +0300

    async javascript tutorial 11 - throwing errors

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,17 @@
 // async and await
 
 const getTodos = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/0");
+
+    if (response.status !== 200) {
+        throw new Error('can not fetch the data');
+    }
+
     const data = await response.json();
 
     return data;
 };
 
-console.log(1);
-console.log(2);
-
 getTodos()
-    .then((data) => console.log("resolved", data));
-
-console.log(3);
-console.log(4);
-
-// fetch("https://jsonplaceholder.typicode.com/todos/1")
-//     .then((response) => {
-//         console.log("resolved", response);
-//         return response.json(); // promise
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch((err) => {
-//         console.log("rejected", err);
-//     });
+    .then((data) => console.log("resolved", data))
+    .catch(err => console.log('rejected', err.message));
