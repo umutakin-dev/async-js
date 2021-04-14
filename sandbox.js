1c6e3ff1539a821bed81f7a683ae4282de59a476
Umut Akin <umutakin@fastmail.com>
2021-04-14 11:19:30 +0300

    async javascript tutorial 10 - async & await

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,29 @@
-// fetch api
+// async and await
 
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((response) => {
-        console.log("resolved", response);
-        return response.json(); // promise
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch((err) => {
-        console.log("rejected", err);
-    });
+const getTodos = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await response.json();
+
+    return data;
+};
+
+console.log(1);
+console.log(2);
+
+getTodos()
+    .then((data) => console.log("resolved", data));
+
+console.log(3);
+console.log(4);
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then((response) => {
+//         console.log("resolved", response);
+//         return response.json(); // promise
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch((err) => {
+//         console.log("rejected", err);
+//     });
